09e3d962181c28e3c70d08deb5b87fae51e59a91
jmnsingle <jmnogueira2011@gmail.com>
2019-11-24 22:11:04 -0300

    Mensagens de alerta com toastify adicionada.

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '~/services/api';
 import history from '~/services/history';
@@ -20,6 +21,8 @@ export function* signIn({ payload }) {
     history.push('/dashboard');
   } catch (err) {
     yield put(signFailure());
+
+    toast.error('Falha na autenticação, verifique seus dados');
   }
 }
 
